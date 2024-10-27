2e44913089ec656536d080c158344cbb4e2354a9
Antonio Jorge <adj.zito@gmail.com>
2024-10-27 23:57:56 +0100

    ppg - setting up a basic level, still not working

--- code/main.py
+++ code/main.py
@@ -1,13 +1,20 @@
 from settings import *
 from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
+import os
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Super Pirate World')
+        
+        # self.tmx_maps = {0: load_pygame(join('..', 'data', 'levels', 'omni.tmx'))}
+        self.tmx_maps = {0: load_pygame(join('..', 'data', 'levels', 'omni.tmx'))}        
+        # self.tmx_maps = os.path.join('../data/levels/omni.tmx')
+        self.current_stage = Level(self.tmx_maps[0])
 
-        self.current_stage = Level()
 
     def run(self):
         while True:
